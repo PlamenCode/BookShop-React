e3f6334ea673f02b8948117534c48f943e6505d0
PlamenCode <plamenggg233@gmail.com>
2023-10-31 09:49:43 +0200

    css changes on the cart component

--- book-shop/src/components/Cart-Book.js
+++ book-shop/src/components/Cart-Book.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 function CartBook( book ){
     return(
-        <div class="card">
-        <div class="container">
-            <img src={book.img} alt="" />
+        <div class="cart">
+        <div class="cart-container">
+            <div className="cart-img">
+                <img src={book.img} alt="" />
+            </div>
+
             <h3>{ book.name }</h3>
             <p>by { book.author}</p>
-            <p>{ book.price }</p>
-            <div class="buttons">
+            <p>${ book.price }</p>
+
+            <div class="card-buttons">
                 <button class="btn fill" >Remove from Cart</button>
                 <button class="btn outline" >Details</button>
             </div>
