c893984519a7f3b8aaa924bcb88d50bf4443fb63
PlamenCode <plamenggg233@gmail.com>
2023-11-06 23:30:06 +0200

    recomended books css changes

--- book-shop/src/components/Recomended-Book.js
+++ book-shop/src/components/Recomended-Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+require('../styles/Recomended-Book.css');
 
 function RecomendedBook(
     book
