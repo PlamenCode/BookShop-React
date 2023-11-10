0bd52fd4c20e516e50b5f5411c13539201093a4b
PlamenCode <plamenggg233@gmail.com>
2023-11-10 11:55:00 +0200

    error handling

--- book-shop/src/components/Error-Alert.js
+++ book-shop/src/components/Error-Alert.js
@@ -3,12 +3,16 @@ require('../styles/Error-Alert.css');
 
 function ErrorAlert(error){
     let errorMsg = '';
-    if(typeof(error) === 'object' ){
-        errorMsg=  Object.values(error.errors);
+
+    if(typeof(error.errors) === 'object' ){
+        errorMsg = Object.values(error.errors).filter(x => x != '');
+    } else {
+        errorMsg = Object.values(error);
     }
+
     return(
         <div className="error-alert">
-            {errorMsg.map(message => <p>{message}</p>)}
+            {errorMsg.map(message => <p key={message.length}>{message}</p>)}
         </div>
     )
 }
--- book-shop/src/pages/Edit.js
+++ book-shop/src/pages/Edit.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import ErrorAlert from '../components/Error-Alert';
 require('../styles/Edit-Create-Form.css');
 
 function Edit(){
@@ -7,10 +8,43 @@ function Edit(){
     const [ inputs, setInputs ] = useState(state);
     const navigate = useNavigate();
 
+    const [hasError, setHasError] = useState(false);
+    const[errors, setErrors] = useState({
+        name: '',
+        author: '',
+        img: '',
+        price: '',
+        description: ''
+    })
+
     const changeHandler = (event) => {
         event.preventDefault();
         const name = event.target.name;
         const value = event.target.value;
+
+        if(name === 'name' && value === ''){
+            setHasError(true)
+            setErrors({...errors, [name]: 'Name is reqired'})
+        } else if(name === 'author' && value === ''){
+            setErrors(true)
+            setErrors({...errors, [name]: 'Author is reqired'})
+        } else if(name === 'img' && value === ''){
+            setHasError(true)
+            setErrors({...errors, [name]: 'IMG is reqired'})
+        } else if(name === 'price' && value < 1 ){
+            setHasError(true)
+            setErrors({...errors, [name]: 'Price must be above 0'})
+        } else if(name === 'description'){
+            if(value.length < 10){
+                setHasError(true)
+                setErrors({...errors, [name]: 'Description must be at least 10 charecters long'})
+            } else{
+                setErrors({...errors, [name]: ''});
+            }
+        } else{
+            setErrors({...errors, [name]: ''});
+        }
+
         setInputs(values => ({...values, [name]: value}));
     };
 
@@ -31,16 +65,19 @@ function Edit(){
             console.log(err);
         })
     };
+
     return(
         <section className="content">
             <h1>Edit your Book Offer</h1>
+
+            {hasError ? <ErrorAlert errors={errors}/> : ''}
 
             <form onSubmit={onEditSubmit}>
                 <div className="input">
                     <label htmlFor="name">Book Name</label>
                     <input type="text" name="name" placeholder="Book name"
                      defaultValue={state.name} 
-                     onChange={changeHandler}/>
+                     onChange={changeHandler} className={errors.name ? 'error' : ''}/>
                 </div>
 
         
@@ -48,28 +85,28 @@ function Edit(){
                     <label htmlFor="author">Author</label>
                     <input type="text" name="author" placeholder="Author" 
                     defaultValue={state.author}
-                    onChange={changeHandler}/>
+                    onChange={changeHandler} className={errors.author ? 'error' : ''}/>
                 </div>
 
                 <div className="input">
                     <label htmlFor="img">Image Url</label>
                     <input type="text" name="img" placeholder="https://..." 
                     defaultValue={state.img}
-                    onChange={changeHandler}/>
+                    onChange={changeHandler} className={errors.img ? 'error' : ''}/>
                 </div>
 
                 <div className="input">
                     <label htmlFor="price">Price</label>
                     <input type="number" name="price" placeholder="Price" 
                     defaultValue={state.price}
-                    onChange={changeHandler}/>
+                    onChange={changeHandler} className={errors.price ? 'error' : ''}/>
                 </div>
 
                 <div className="input">
                     <label htmlFor="description">Description</label>
                     <textarea type="text" name="description" placeholder="Description"
                     defaultValue={state.description}
-                    onChange={changeHandler}></textarea>
+                    onChange={changeHandler} className={errors.description ? 'error' : ''}></textarea>
                 </div>
         
         
