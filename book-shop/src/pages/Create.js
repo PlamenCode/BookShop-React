306421fcd13583d92f2050b13e19f4084c64a2a4
PlamenCode <plamenggg233@gmail.com>
2023-11-11 20:52:15 +0200

    error handling on creating and editing complete

--- book-shop/src/pages/Create.js
+++ book-shop/src/pages/Create.js
@@ -4,12 +4,12 @@ import ErrorAlert from '../components/Error-Alert';
 require('../styles/Edit-Create-Form.css');
 
 
-function Create(){
+function Create() {
     const navigate = useNavigate();
-    const [ inputs, setInputs ] = useState({});
+    const [inputs, setInputs] = useState({});
 
     const [hasError, setHasError] = useState(false);
-    const[errors, setErrors] = useState({
+    const [errors, setErrors] = useState({
         name: '',
         author: '',
         img: '',
@@ -20,20 +20,28 @@ function Create(){
 
     const onCreateSubmit = (event) => {
         event.preventDefault();
-        const options = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ book: inputs, user: localStorage.getItem('userId') })
+        if (!inputs.name || inputs.name == '' ||
+            !inputs.author || inputs.author == '' ||
+            !inputs.img || inputs.img == '' ||
+            !inputs.price || inputs.price == '' ||
+            !inputs.description || inputs.description == '') {
+            return;
+        } else {
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ book: inputs, user: localStorage.getItem('userId') })
+            }
+            fetch('http://localhost:4200/ReactDef/data', options)
+                .then(res => res.json())
+                .then(data => {
+                    setInputs({});
+                    navigate(`/books/${data._id}`);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
-        fetch('http://localhost:4200/ReactDef/data', options)
-            .then(res => res.json())
-            .then(data => {
-                setInputs({});
-                navigate(`/books/${data._id}`);
-            })
-            .catch(err => {
-                console.log(err);
-            })
     };
 
     const changeHandler = (event) => {
@@ -41,73 +49,73 @@ function Create(){
         const name = event.target.name;
         const value = event.target.value.trim();
 
-        if(name === 'name' && value === ''){
+        if (name === 'name' && value === '') {
             setHasError(true)
-            setErrors({...errors, [name]: 'Name is reqired'})
-        } else if(name === 'author' && value === ''){
+            setErrors({ ...errors, [name]: 'Name is reqired' })
+        } else if (name === 'author' && value === '') {
             setErrors(true)
-            setErrors({...errors, [name]: 'Author is reqired'})
-        } else if(name === 'img' && value === ''){
+            setErrors({ ...errors, [name]: 'Author is reqired' })
+        } else if (name === 'img' && value === '') {
             setHasError(true)
-            setErrors({...errors, [name]: 'IMG is reqired'})
-        } else if(name === 'price' && value < 1 ){
+            setErrors({ ...errors, [name]: 'IMG is reqired' })
+        } else if (name === 'price' && value < 1) {
             setHasError(true)
-            setErrors({...errors, [name]: 'Price must be above 0'})
-        } else if(name === 'description'){
-            if(value.length < 10){
+            setErrors({ ...errors, [name]: 'Price must be above 0' })
+        } else if (name === 'description') {
+            if (value.length < 10) {
                 setHasError(true)
-                setErrors({...errors, [name]: 'Description must be at least 10 charecters long'})
-            } else{
-                setErrors({...errors, [name]: ''});
+                setErrors({ ...errors, [name]: 'Description must be at least 10 charecters long' })
+            } else {
+                setErrors({ ...errors, [name]: '' });
             }
-        } else{
-            setErrors({...errors, [name]: ''});
+        } else {
+            setErrors({ ...errors, [name]: '' });
         }
-        setInputs(values => ({...values, [name]: value}));
+        setInputs(values => ({ ...values, [name]: value }));
     }
 
 
-        return(
-            <section className="content">
-                <h1>Create your Book Offer</h1>
-                {hasError ? <ErrorAlert errors={errors}/> : ''}
-    
-                <form onSubmit={onCreateSubmit}>
-                    <div className="input">
-                        <label htmlFor="name">Book Name</label>
-                        <input type="text" name="name" placeholder="Book name" 
-                        onChange={changeHandler} className={errors.name ? 'error' : ''}/>
-                    </div>
-    
-            
-                    <div className="input">
-                        <label htmlFor="author">Author</label>
-                        <input type="text" name="author" placeholder="Author" 
-                        onChange={changeHandler} className={errors.author ? 'error' : ''}/>
-                    </div>
-    
-                    <div className="input">
-                        <label htmlFor="img">Image Url</label>
-                        <input type="text" name="img" placeholder="https://..." 
-                        onChange={changeHandler} className={errors.img ? 'error' : ''}/>
-                    </div>
-    
-                    <div className="input">
-                        <label htmlFor="price">Price</label>
-                        <input type="number" name="price" placeholder="Price" 
-                        onChange={changeHandler} className={errors.price ? 'error' : ''}/>
-                    </div>
-    
-                    <div className="input">
-                        <label htmlFor="description">Description</label>
-                        <textarea type="text" name="description" placeholder="Description" 
+    return (
+        <section className="content">
+            <h1>Create your Book Offer</h1>
+            {hasError ? <ErrorAlert errors={errors} /> : ''}
+
+            <form onSubmit={onCreateSubmit}>
+                <div className="input">
+                    <label htmlFor="name">Book Name</label>
+                    <input type="text" name="name" placeholder="Book name"
+                        onChange={changeHandler} className={errors.name ? 'error' : ''} />
+                </div>
+
+
+                <div className="input">
+                    <label htmlFor="author">Author</label>
+                    <input type="text" name="author" placeholder="Author"
+                        onChange={changeHandler} className={errors.author ? 'error' : ''} />
+                </div>
+
+                <div className="input">
+                    <label htmlFor="img">Image Url</label>
+                    <input type="text" name="img" placeholder="https://..."
+                        onChange={changeHandler} className={errors.img ? 'error' : ''} />
+                </div>
+
+                <div className="input">
+                    <label htmlFor="price">Price</label>
+                    <input type="number" name="price" placeholder="Price"
+                        onChange={changeHandler} className={errors.price ? 'error' : ''} />
+                </div>
+
+                <div className="input">
+                    <label htmlFor="description">Description</label>
+                    <textarea type="text" name="description" placeholder="Description"
                         onChange={changeHandler} className={errors.description ? 'error' : ''} ></textarea>
-                    </div>
-            
-            
-                    <button type="submit" disabled={ hasError }>Post Your Offer</button>
-                </form>
-            </section>
+                </div>
+
+
+                <button type="submit">Post Your Offer</button>
+            </form>
+        </section>
     )
 }
 
--- book-shop/src/pages/Edit.js
+++ book-shop/src/pages/Edit.js
@@ -1,119 +1,163 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import ErrorAlert from '../components/Error-Alert';
-require('../styles/Edit-Create-Form.css');
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import ErrorAlert from "../components/Error-Alert";
+require("../styles/Edit-Create-Form.css");
 
-function Edit(){
+function Edit() {
     const { state } = useLocation();
-    const [ inputs, setInputs ] = useState(state);
+    const [inputs, setInputs] = useState(state);
     const navigate = useNavigate();
 
     const [hasError, setHasError] = useState(false);
-    const[errors, setErrors] = useState({
-        name: '',
-        author: '',
-        img: '',
-        price: '',
-        description: ''
-    })
+    const [errors, setErrors] = useState({
+        name: "",
+        author: "",
+        img: "",
+        price: "",
+        description: "",
+    });
 
     const changeHandler = (event) => {
         event.preventDefault();
         const name = event.target.name;
         const value = event.target.value;
 
-        if(name === 'name' && value === ''){
-            setHasError(true)
-            setErrors({...errors, [name]: 'Name is reqired'})
-        } else if(name === 'author' && value === ''){
-            setErrors(true)
-            setErrors({...errors, [name]: 'Author is reqired'})
-        } else if(name === 'img' && value === ''){
-            setHasError(true)
-            setErrors({...errors, [name]: 'IMG is reqired'})
-        } else if(name === 'price' && value < 1 ){
-            setHasError(true)
-            setErrors({...errors, [name]: 'Price must be above 0'})
-        } else if(name === 'description'){
-            if(value.length < 10){
-                setHasError(true)
-                setErrors({...errors, [name]: 'Description must be at least 10 charecters long'})
-            } else{
-                setErrors({...errors, [name]: ''});
+        if (name === "name" && value === "") {
+            setHasError(true);
+            setErrors({ ...errors, [name]: "Name is reqired" });
+        } else if (name === "author" && value === "") {
+            setErrors(true);
+            setErrors({ ...errors, [name]: "Author is reqired" });
+        } else if (name === "img" && value === "") {
+            setHasError(true);
+            setErrors({ ...errors, [name]: "IMG is reqired" });
+        } else if (name === "price" && value < 1) {
+            setHasError(true);
+            setErrors({ ...errors, [name]: "Price must be above 0" });
+        } else if (name === "description") {
+            if (value.length < 10) {
+                setHasError(true);
+                setErrors({
+                    ...errors,
+                    [name]: "Description must be at least 10 charecters long",
+                });
+            } else {
+                setErrors({ ...errors, [name]: "" });
             }
-        } else{
-            setErrors({...errors, [name]: ''});
+        } else {
+            setErrors({ ...errors, [name]: "" });
         }
 
-        setInputs(values => ({...values, [name]: value}));
+        setInputs((values) => ({ ...values, [name]: value }));
     };
 
-    function onEditSubmit(event){
+    function onEditSubmit(event) {
         event.preventDefault();
-        const options = {
-            method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ book: inputs, user: localStorage.getItem('userId') })
-        };
-        fetch(`http://localhost:4200/ReactDef/data/${state._id}`, options)
-        .then(res => res.json())
-        .then(data => {
-            setInputs({});
-            navigate(`/books/${data._id}`);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    };
+        if (
+            !inputs.name ||
+            inputs.name == "" ||
+            !inputs.author ||
+            inputs.author == "" ||
+            !inputs.img ||
+            inputs.img == "" ||
+            !inputs.price ||
+            inputs.price == "" ||
+            !inputs.description ||
+            inputs.description == ""
+        ) {
+            return;
+        } else {
+            const options = {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    book: inputs,
+                    user: localStorage.getItem("userId"),
+                }),
+            };
+            fetch(`http://localhost:4200/ReactDef/data/${state._id}`, options)
+                .then((res) => res.json())
+                .then((data) => {
+                    setInputs({});
+                    navigate(`/books/${data._id}`);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
 
-    return(
+    return (
         <section className="content">
             <h1>Edit your Book Offer</h1>
 
-            {hasError ? <ErrorAlert errors={errors}/> : ''}
+            {hasError ? <ErrorAlert errors={errors} /> : ""}
 
             <form onSubmit={onEditSubmit}>
                 <div className="input">
                     <label htmlFor="name">Book Name</label>
-                    <input type="text" name="name" placeholder="Book name"
-                     defaultValue={state.name} 
-                     onChange={changeHandler} className={errors.name ? 'error' : ''}/>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Book name"
+                        defaultValue={state.name}
+                        onChange={changeHandler}
+                        className={errors.name ? "error" : ""}
+                    />
                 </div>
 
-        
                 <div className="input">
                     <label htmlFor="author">Author</label>
-                    <input type="text" name="author" placeholder="Author" 
-                    defaultValue={state.author}
-                    onChange={changeHandler} className={errors.author ? 'error' : ''}/>
+                    <input
+                        type="text"
+                        name="author"
+                        placeholder="Author"
+                        defaultValue={state.author}
+                        onChange={changeHandler}
+                        className={errors.author ? "error" : ""}
+                    />
                 </div>
 
                 <div className="input">
                     <label htmlFor="img">Image Url</label>
-                    <input type="text" name="img" placeholder="https://..." 
-                    defaultValue={state.img}
-                    onChange={changeHandler} className={errors.img ? 'error' : ''}/>
+                    <input
+                        type="text"
+                        name="img"
+                        placeholder="https://..."
+                        defaultValue={state.img}
+                        onChange={changeHandler}
+                        className={errors.img ? "error" : ""}
+                    />
                 </div>
 
                 <div className="input">
                     <label htmlFor="price">Price</label>
-                    <input type="number" name="price" placeholder="Price" 
-                    defaultValue={state.price}
-                    onChange={changeHandler} className={errors.price ? 'error' : ''}/>
+                    <input
+                        type="number"
+                        name="price"
+                        placeholder="Price"
+                        defaultValue={state.price}
+                        onChange={changeHandler}
+                        className={errors.price ? "error" : ""}
+                    />
                 </div>
 
                 <div className="input">
                     <label htmlFor="description">Description</label>
-                    <textarea type="text" name="description" placeholder="Description"
-                    defaultValue={state.description}
-                    onChange={changeHandler} className={errors.description ? 'error' : ''}></textarea>
+                    <textarea
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        defaultValue={state.description}
+                        onChange={changeHandler}
+                        className={errors.description ? "error" : ""}
+                    ></textarea>
                 </div>
-        
-        
+
                 <button type="submit">Edit Your Offer</button>
             </form>
         </section>
-    )
+    );
 }
 
 export default Edit;
